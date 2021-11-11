52702b1ac96097244af3609191313b16cf1ca90f
Mansi Ganar <86571822+MansiGanar@users.noreply.github.com>
2021-11-11 15:23:21 +0100

    Styling the cards

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,25 +1,42 @@
-import { Card, Row } from "react-bootstrap";
+import { Col, Container, Form, Row } from "react-bootstrap";
+import React from "react";
+import SingleBook from "./SingleBook";
 
-const BookList = (props) => {
-  return (
-    <>
-      <Row>
-        {props.book.map((bookObject) => (
-          <Card
-            xs={12}
-            md={3}
-            lg={2}
-            style={{ width: "18rem", padding: "20px", margin: "10px" }}
-          >
-            <Card.Img variant="top" src={bookObject.img} />
-            <Card.Body>
-              <Card.Title>{bookObject.title}</Card.Title>
-            </Card.Body>
-          </Card>
-        ))}
-      </Row>
-    </>
-  );
-};
+class BookList extends React.Component {
+  state = {
+    searchQuery: "",
+  };
+  render() {
+    console.log("Hey there");
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Label className="text-white">Search</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Search here"
+                value={this.state.searchQuery}
+                onChange={(e) => this.setState({ searchQuery: e.target.value })}
+              />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Row>
+          {this.props.book
+            .filter((b) =>
+              b.title.toLowerCase().includes(this.state.searchQuery)
+            )
+            .map((b) => (
+              <Col xs={3} key={b.asin}>
+                <SingleBook book={b} />
+              </Col>
+            ))}
+        </Row>
+      </Container>
+    );
+  }
+}
 
 export default BookList;
--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -7,48 +7,30 @@ class SingleBook extends Component {
   };
   render() {
     return (
-      <Card>
+      <Card
+        className="BookCards"
+        onClick={() => {
+          this.setState({ selectedBook: !this.state.selectedBook });
+          console.log(this.state);
+        }}
+        style={{
+          cursor: "pointer",
+          width: "15rem",
+          border: this.state.selectedBook ? "3px solid red" : "1px solid black",
+        }}
+      >
         <Card.Img
-          className="d-flex justify-content-center"
+          className=" ml-5 mt-2"
+          style={{ width: "150px" }}
           variant="top"
-          onClick={() => {
-            this.setState({ selectedBook: !this.state.selectedBook });
-            console.log(this.state);
-          }}
           src={this.props.book.img}
           key={this.props.asin}
-          style={{
-            cursor: "pointer",
-            width: "15rem",
-            border: this.state.selectedBook
-              ? "3px solid red"
-              : "1px solid black",
-          }}
         />
         <Card.Body>
           <Card.Title>{this.props.book.title}</Card.Title>
-          <Button variant="warning">Buy</Button>
         </Card.Body>
       </Card>
     );
   }
 }
-
-/* const SingleBookCard = (props) => {
-  return (
-    <Card>
-      <Card.Img
-        className="d-flex justify-content-center"
-        variant="top"
-        src={props.book.img}
-        key={props.asin}
-        style={{ width: "15rem" }}
-      />
-      <Card.Body>
-        <Card.Title>{props.book.title}</Card.Title>
-        <Button variant="warning">Buy</Button>
-      </Card.Body>
-    </Card>
-  );
-}; */
 export default SingleBook;
